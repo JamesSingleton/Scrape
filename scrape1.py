715e18bcaf2eeda280e760a254adf04602045888
DrSarge <Marines37>
2017-02-16 01:00:18 -0800

    Getting number of pages and interating through them

--- scrape1.py
+++ scrape1.py
@@ -1,18 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-target_url ="your url here"
+base_url="your url here"
+ar = requests.get(base_url)
+soupBase = BeautifulSoup(ar.content, 'html.parser')
+pagination = soupBase.find_all("div", {"class": "paging"})
 
-r = requests.get(target_url)
+for number in pagination:
+    pages = number.find_all("a")[6]
+    newPages = pages.get("data-page")
+    intPages = int(newPages)
+    #print (intPages)
 
-soup = BeautifulSoup(r.content, 'html.parser')
+for i in range(intPages+1):
+    try:
+        target_url =base_url+"{}".format(i)
+        print (target_url)
+        r = requests.get(target_url)
+        soupTarget = BeautifulSoup(r.content, 'html.parser')
+        g_data = soupTarget.find_all("article", {"class": "placard"})
+        for item in g_data:
+            #print (item.contents)
+            try:
+                print (item.find_all("a", {"class": "placardTitle"})[0].text)
+                print (item.find_all("div", {"class": "location"})[0].text, '\n')
+            except:
+                pass
+    except:
+        pass
 
-# for link in soup.find_all('a'):
-#     print(link.text,link.get('href'))
-
-g_data = soup.find_all("article", {"class": "diamond"})
-
-for item in g_data:
-    #print (item.contents)
-    print (item.find_all("a", {"class": "placardTitle"})[0].text)
-    print (item.find_all("div", {"class": "location"})[0].text, '\n')
+print('done!')
